62993fdb04a41535a7a869fbbe3ef6fca527baf6
TheRealVira <Thomas.Ruehrig@studierende.htl-donaustadt.at>
2018-09-07 20:06:23 +0200

    git does not seem to work properly on my machine..

---
+++ src/picstamp/number/Pair.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package picstamp.number;
+
+/**
+ *
+ * @author vira
+ */
+class Pair<T, U> {         
+    public T t;
+    public U u;
+
+    public Pair(T t, U u) {         
+        this.t= t;
+        this.u= u;
+     }
+}
